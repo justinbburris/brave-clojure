7a67719aa081bf769af5d0ed851ab69a9b790384
Justin Burris <justinbburris@gmail.com>
2014-06-24 13:19:49 +0800

    remove target as it's already in scope

--- src/brave_clojure/core.clj
+++ src/brave_clojure/core.clj
@@ -65,16 +65,16 @@
         part
         (recur rest (+ accumulated-size (:size part)))))))
 
-(defn janky-hit
+;; for each part, add its size to the accumulated-size and return
+;; the part if the accumulated-size is greater than the target
+(defn hit-annoymous-function
+  "Hit one of the hobbits body parts."
   [asym-body-parts]
   (let [sym-parts (better-symmetrize-body-parts asym-body-parts)
         body-part-size-sum (reduce + 0 (map :size sym-parts))
         target (inc (rand body-part-size-sum))]
-    ((fn [accumulated-size target sym-parts]
+    ((fn [accumulated-size sym-parts]
        (let [[part & sym-parts] sym-parts]
          (if (> accumulated-size target)
            part
-           (recur (+ accumulated-size (:size part)) target sym-parts)))) 0 target sym-parts)))
-
-;; for each part, add its size to the accumulated-size and return
-;; the part if the accumulated-size is greater than the target
+           (recur (+ accumulated-size (:size part)) sym-parts)))) 0 sym-parts)))
